ef92e373e4291478aa77aa6831c0205b5a3ca623
Liam Miller <lgm5590@g.rit.edu>
2017-12-02 12:19:41 -0500

    Base layout started

--- app/src/main/java/com/example/liam/tamagotchiwaifu/MainActivity.java
+++ app/src/main/java/com/example/liam/tamagotchiwaifu/MainActivity.java
@@ -2,12 +2,40 @@ package com.example.liam.tamagotchiwaifu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LinearLayout lotLove = findViewById(R.id.lotLove);
+        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(100, 100);
+        for(int i = 0; i < 6; i++)
+        {
+            ImageView vw = new ImageView(getApplicationContext());
+            if(i == 0)
+            {
+                vw.setImageResource(R.mipmap.love_half);
+            }
+            else
+            {
+                vw.setImageResource(R.mipmap.love_empty);
+            }
+            vw.setLayoutParams(params);
+            lotLove.addView(vw, i);
+        }
     }
 }
