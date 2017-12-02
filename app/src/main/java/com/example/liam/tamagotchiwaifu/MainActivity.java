6229b339c888c35f3cdf63b0a52a472a184bf950
Liam Miller <lgm5590@g.rit.edu>
2017-12-02 12:35:38 -0500

    Added placeholder buttons

--- app/src/main/java/com/example/liam/tamagotchiwaifu/MainActivity.java
+++ app/src/main/java/com/example/liam/tamagotchiwaifu/MainActivity.java
@@ -23,19 +23,54 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         LinearLayout lotLove = findViewById(R.id.lotLove);
         ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(100, 100);
+        //initialize hearts
         for(int i = 0; i < 6; i++)
         {
             ImageView vw = new ImageView(getApplicationContext());
-            if(i == 0)
-            {
-                vw.setImageResource(R.mipmap.love_half);
-            }
-            else
-            {
-                vw.setImageResource(R.mipmap.love_empty);
-            }
+            vw.setImageResource(R.mipmap.love_empty);
             vw.setLayoutParams(params);
             lotLove.addView(vw, i);
+        }
+        params = new ViewGroup.LayoutParams(50, 50);
+
+        //initialize food
+        LinearLayout lotFood = findViewById(R.id.lotFood);
+        for(int i = 0; i < 6; i++)
+        {
+            ImageView vw = new ImageView(getApplicationContext());
+            vw.setImageResource(R.mipmap.hunger_empty);
+            vw.setLayoutParams(params);
+            lotFood.addView(vw, i);
+        }
+
+        //initialize confidence
+        LinearLayout lotConfidence = findViewById(R.id.lotConfidence);
+        for(int i = 0; i < 6; i++)
+        {
+            ImageView vw = new ImageView(getApplicationContext());
+            vw.setImageResource(R.mipmap.confident_empty);
+            vw.setLayoutParams(params);
+            lotConfidence.addView(vw, i);
+        }
+
+        //initialize Health
+        LinearLayout lotHealth = findViewById(R.id.lotHealth);
+        for(int i = 0; i < 6; i++)
+        {
+            ImageView vw = new ImageView(getApplicationContext());
+            vw.setImageResource(R.mipmap.health_empty);
+            vw.setLayoutParams(params);
+            lotHealth.addView(vw, i);
+        }
+
+        //initialize Happiness
+        LinearLayout lotHappy = findViewById(R.id.lotHappy);
+        for(int i = 0; i < 6; i++)
+        {
+            ImageView vw = new ImageView(getApplicationContext());
+            vw.setImageResource(R.mipmap.happy_empty);
+            vw.setLayoutParams(params);
+            lotHappy.addView(vw, i);
         }
     }
 }
